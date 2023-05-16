c9f8ee0fe2737438c83f22ca31158491aadd111b
Manas Pasayat <manaspasayat3@gmail.com>
2023-05-16 15:22:22 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import About from "./About";
 function App() {
   return (
     <div className="App">
-      <Routes>
+      <h2> APP Page</h2>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
